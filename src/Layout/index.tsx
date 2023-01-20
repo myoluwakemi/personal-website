696701d7156e4968462016312fc3f1664411c295
oluwakemi <yusufoluwakemi39@gmail.com>
2023-01-20 20:50:26 +0100

    updated index.html for resizable favicon

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+
 import Header from "../components/Header";
 import { Wrapper } from "./styles";
 
-const Layout = ()=> {
-    return(
-        <Wrapper>
-            <Header/>
-            <Outlet/>
-        </Wrapper>
-    )
-}
-export default Layout
+const Layout = () => {
+
+  return (
+    <Wrapper>
+      <Header />
+
+      <Outlet />
+    </Wrapper>
+  );
+};
+export default Layout;
